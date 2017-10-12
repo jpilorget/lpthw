8c0b189252c19c2d971399db8adc609cdc32ce41
jpilorget <jpilorget@gmail.com>
2017-10-12 16:35:39 -0300

    Create ex9.py

---
+++ ex9.py
@@ -0,0 +1,17 @@
+#Here's somw new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+#I add a newline before January
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+#If I add more quotes at the beginning they just get printed
+print("""
+There's something going on here.
+Whith the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
